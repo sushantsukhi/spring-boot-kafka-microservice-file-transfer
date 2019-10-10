096351ce86b18f2d68070835ddede70ee61d6a9f
sushantsukhi <sushantsukhi@gmail.com>
2019-10-10 19:39:09 +0530

    Committing changes for multi threading producer changes

--- spring-boot-kafka-producer-file/src/main/java/com/happycoding/config/KafkaProducerConfiguration.java
+++ spring-boot-kafka-producer-file/src/main/java/com/happycoding/config/KafkaProducerConfiguration.java
@@ -2,6 +2,7 @@ package com.happycoding.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Executor;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -12,10 +13,13 @@ import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import com.happycoding.model.Provider;
 
 @Configuration
+@EnableAsync
 public class KafkaProducerConfiguration {
 
 	@Value("${bootstrap.servers}")
@@ -33,6 +37,17 @@ public class KafkaProducerConfiguration {
 	@Bean
 	public KafkaTemplate<String, Provider> kafkaTemplate() {
 		return new KafkaTemplate<String, Provider>(producerFactory());
+	}
+
+	@Bean(name = "taskExecutor")
+	public Executor taskExecutor() {
+		final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("ProducerThread-");
+		executor.initialize();
+		return executor;
 	}
 
 }
