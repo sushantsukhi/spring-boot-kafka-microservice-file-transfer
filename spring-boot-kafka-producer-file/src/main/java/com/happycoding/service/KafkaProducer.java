c996eff424340c2271dfc24c2adcec6ea3a77ceb
sushantsukhi <sushantsukhi@gmail.com>
2019-10-10 19:40:50 +0530

    Committing changes for multi threading producer changes

--- spring-boot-kafka-producer-file/src/main/java/com/happycoding/service/KafkaProducer.java
+++ spring-boot-kafka-producer-file/src/main/java/com/happycoding/service/KafkaProducer.java
@@ -3,6 +3,7 @@ package com.happycoding.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.happycoding.model.Provider;
@@ -16,8 +17,18 @@ public class KafkaProducer {
 	@Value("${topic.id}")
 	private String topicId;
 
+	@Async
 	public void send(String message, String fileName, Integer value) {
-		kafkaTemplate.send(topicId, fileName, new Provider(message, value));
+		System.out.println("Current thread used for sending message to topic :" + Thread.currentThread().getName());
+		kafkaTemplate.send(topicId, fileName, new Provider(fileName, value, message));
+		System.out.println("Message sent to the Kafka Topic '" + topicId + "' Successfully");
+	}
+	
+	
+	@Async
+	public void send(String message, String fileName, Integer value, String keyValue) {
+		System.out.println("Current thread used for sending message to topic :" + Thread.currentThread().getName());
+		kafkaTemplate.send(topicId, keyValue, new Provider(fileName, value, message));
 		System.out.println("Message sent to the Kafka Topic '" + topicId + "' Successfully");
 	}
 }
